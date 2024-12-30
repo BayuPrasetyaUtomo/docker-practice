85225efaf00b90d968428a118aed0876cbfc0912
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-12-30 19:10:28 +0700

    fix: apply temporary timezone for timestamp

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 HOST_TIMESTAMP=$(date +%F_%H-%M-%S)
-SINGAPORE_TIMESTAMP=$(TZ=Asia/Singapore date +%F_%H-%M-%S)
+SINGAPORE_TIMESTAMP=$(env TZ=Asia/Singapore date +%F_%H-%M-%S)
 
 echo "$GREETING $NAME, currently it's $HOST_TIMESTAMP in the host timezone and $SINGAPORE_TIMESTAMP in Asia/Singapore timezone when this script is running"
