d40480e7e51039b05478e6b391cfb1ece581b350
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-12-30 19:05:23 +0700

    fix: placement of TZ

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 HOST_TIMESTAMP=$(date +%F_%H-%M-%S)
-TZ=Asia/Singapore SINGAPORE_TIMESTAMP=$(date +%F_%H-%M-%S)
+SINGAPORE_TIMESTAMP=$(TZ=Asia/Singapore date +%F_%H-%M-%S)
 
 echo "$GREETING $NAME, currently it's $HOST_TIMESTAMP in the host timezone and $SINGAPORE_TIMESTAMP in Asia/Singapore timezone when this script is running"
