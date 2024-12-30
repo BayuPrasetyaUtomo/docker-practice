4d564c99a4fa853645236aebea16e3df09237e49
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-12-30 19:21:43 +0700

    fix: attempt to show the correct alternative timezone

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HOST_TIMESTAMP=$(date +%F_%H-%M-%S)
-SINGAPORE_TIMESTAMP=$(env TZ=Asia/Singapore date +%F_%H-%M-%S)
+HOST_TIMESTAMP=$(date +%FI_%H-%M-%S)
+SINGAPORE_TIMESTAMP=$(TZ=Asia/Jakarta date +%FI_%H-%M-%S)
 
-echo "$GREETING $NAME, currently it's $HOST_TIMESTAMP in the host timezone and $SINGAPORE_TIMESTAMP in Asia/Singapore timezone when this script is running"
+echo "${GREETING} ${NAME}, currently it's ${HOST_TIMESTAMP} in the host timezone and ${SINGAPORE_TIMESTAMP} in Asia/Singapore timezone when this script is running"
